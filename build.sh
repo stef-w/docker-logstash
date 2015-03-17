de3e7738f254499ac46ab652310c0c787087b45f
Brian Lalor <brian@autosportlabs.com>
2015-03-17 07:42:37 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-set -e
+set -e -u
 
-IMAGE="zardoz.podzone.org:11003/apps/logstash"
+DOCKER_REGISTRY="${1}"
+PARENT_IMAGE="${2:-}"
 
-docker build -rm -t ${IMAGE} .
-docker push ${IMAGE}
+image="${DOCKER_REGISTRY}/apps/logstash"
+
+tag="$( git describe --always --dirty )"
+if [ -n "${BUILD_NUMBER:-}" ]; then
+    tag="${tag}-${BUILD_NUMBER}"
+fi
+
+sed -i -e "/^FROM/cFROM ${PARENT_IMAGE}" Dockerfile
+docker pull "${PARENT_IMAGE}"
+
+docker build -t "${image}:${tag}" .
+docker tag --force "${image}:${tag}" "${image}:latest"
+docker push "${image}:${tag}"
+docker push "${image}:latest"
+docker rmi --force $( docker inspect --format '{{ .Id }}' "${image}:${tag}" )
+
+echo "image=${image}" >| docker_build.properties
+echo "tag=${tag}" >> docker_build.properties
